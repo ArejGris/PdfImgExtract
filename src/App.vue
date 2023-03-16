1ca17621aea3a530cd840db35d81f859bcaa62a7
ArejGris <mairygris1234@gmail.com>
2023-03-16 07:14:48 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ImageExtract/>
+</template>
+
+<script>
+import ImageExtract from './components/ImageExtract.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ImageExtract
+  },
+  data(){
+    return{
+      show:'true'
+    }
+  },
+  methods:{
+    clic(){
+      this.show='false'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+//import Vue from 'vue'
+//import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+//import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/css/bootstrap-vue.css'
+import "@fortawesome/fontawesome-svg-core"
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app=createApp(App)
+//Vue.use(BootstrapVue)
+//Vue.use(IconsPlugin)
+app.mount('#app')
