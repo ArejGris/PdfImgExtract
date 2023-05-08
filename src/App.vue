08f39d18bc0244ff80ffe2660ad963f40051edf5
ArejGris <mairygris1234@gmail.com>
2023-05-08 07:59:31 -0700

    git commit

--- src/App.vue
+++ src/App.vue
@@ -40,18 +40,83 @@ export default {
   background-size: cover;
   background-position: center;
 }
-@media screen and (max-width: 760px ) and (min-width: 401px){
+@media screen and (min-height:800px)
+ {
+#app{
+  background: url('../src/assets/img/700-900.svg') no-repeat center center /cover;
+}
+  
+}
+@media screen and (max-height:800px) and (min-width: 1100px) 
+ {
+#app{
+  background: url('../src/assets/img/background.svg') no-repeat center center /cover;
+}
+  
+}
+
+@media screen and (max-width:1100px) and (min-width:771px) and (max-height:900px){
+#app{
+  background: url('../src/assets/img/900-700.svg') no-repeat center center /cover;
+}
+  
+}
+@media screen and (max-width:950px) and (min-width:771px) and (max-height:700px){
+#app{
+  background: url('../src/assets/img/800-500.svg') no-repeat center center /cover;
+}
+  
+}
+@media screen and (max-width:770px ) and (min-width: 551px) 
+{
   #app{
-  background-image: url("./assets/img/midmobilebg.png");
+  background-image: url("./assets/img/700-900.svg");
   background-attachment: fixed;
   position: fixed;
   background-position: center;
   background-size: cover;
   }
 }
-@media screen and (max-width:400px ) {
+@media screen and (max-width:770px ) and (min-width: 551px) and (max-height:750px)
+{
+  #app{
+  background-image: url("./assets/img/800-900.svg");
+  background-attachment: fixed;
+  position: fixed;
+  background-position: center;
+  background-size: cover;
+  }
+}
+@media screen and (max-width: 770px) and (min-width: 551px) and (max-height:600px)
+{
+  #app{
+    background: url("./assets/img/900-700.svg")no-repeat center center /cover;
+    background-attachment: fixed;
+    position: fixed;
+  }
+}
+@media screen and (max-width:550px ) {
   #app{
   background: url("./assets/img/mobilebg.svg") no-repeat center center /cover;
+  background-attachment: fixed;
+  position: fixed;
+
+  }
+}
+
+@media screen and (max-width:550px ) and (max-height:820px)
+{
+  #app{
+  background: url("./assets/img/600-900.svg") no-repeat center center /cover;
+  background-attachment: fixed;
+  position: fixed;
+
+  }
+}
+@media screen and (max-width:550px ) and (max-height:520px)
+{
+  #app{
+  background: url("./assets/img/900-700.svg") no-repeat center center /cover;
   background-attachment: fixed;
   position: fixed;
 
